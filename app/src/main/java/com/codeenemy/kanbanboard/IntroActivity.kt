b955dfd066cb0f8418f5659ada834232983aa4bd
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-09-06 23:44:40 +0530

    Formatted Code

--- app/src/main/java/com/codeenemy/kanbanboard/IntroActivity.kt
+++ app/src/main/java/com/codeenemy/kanbanboard/IntroActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import com.codeenemy.kanbanboard.databinding.ActivityIntroBinding
+
 class IntroActivity : AppCompatActivity() {
     private var binding: ActivityIntroBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
