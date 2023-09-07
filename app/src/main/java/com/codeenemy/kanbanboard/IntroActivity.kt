710974d89bc10ad5237fe9bf1e811f64c2f926c7
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-09-08 01:11:59 +0530

    Created SignIn User Interface

--- app/src/main/java/com/codeenemy/kanbanboard/IntroActivity.kt
+++ app/src/main/java/com/codeenemy/kanbanboard/IntroActivity.kt
@@ -1,5 +1,6 @@
 package com.codeenemy.kanbanboard
 
+import android.content.Intent
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,6 +16,13 @@ class IntroActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         val typeFace: Typeface = Typeface.createFromAsset(assets, "charlie-display-bold.ttf")
         binding?.tvAppName?.typeface = typeFace
+
+        binding?.btnSignUp?.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+        binding?.btnLogIn?.setOnClickListener {
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
     }
 }
 
--- app/src/main/java/com/codeenemy/kanbanboard/SignInActivity.kt
+++ app/src/main/java/com/codeenemy/kanbanboard/SignInActivity.kt
@@ -2,25 +2,25 @@ package com.codeenemy.kanbanboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import com.codeenemy.kanbanboard.databinding.ActivitySignInBinding
 import com.codeenemy.kanbanboard.databinding.ActivitySignUpBinding
+import com.codeenemy.kanbanboard.databinding.SignInActivityBinding
 
-class SignUpActivity : AppCompatActivity() {
-    private var binding: ActivitySignUpBinding? = null
+class SignInActivity : AppCompatActivity() {
+    private var binding: ActivitySignInBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding?.root)
         setupActionBar()
     }
-
     private fun setupActionBar() {
-        setSupportActionBar(binding?.toolbarSignUpActivity)
+        setSupportActionBar(binding?.toolbarSignInActivity)
         val actionBar = supportActionBar
         if (actionBar!= null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         }
-        binding?.toolbarSignUpActivity?.setNavigationOnClickListener { onBackPressed() }
+        binding?.toolbarSignInActivity?.setNavigationOnClickListener { onBackPressed() }
     }
 }
