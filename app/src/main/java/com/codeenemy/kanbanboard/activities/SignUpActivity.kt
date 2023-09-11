b9260703fa70e6cad09d8900cb3129d033a321e1
Harsh Singh <89487399+harshsingh-io@users.noreply.github.com>
2023-09-11 23:53:28 +0530

    refracted the when conditions for sign up activity

--- app/src/main/java/com/codeenemy/kanbanboard/activities/SignUpActivity.kt
+++ app/src/main/java/com/codeenemy/kanbanboard/activities/SignUpActivity.kt
@@ -55,7 +55,7 @@ class SignUpActivity : BaseActivity() {
                 showErrorSnackBar("Please enter a password")
                 false
             } else -> {
-                return true
+                true
             }
         }
     }
